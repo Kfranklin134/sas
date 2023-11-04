a0306877fd1aa07e7a4f732a8924dc228872ce3c
Kevin Franklin <kevinmfranklin@students.abtech.edu>
2023-11-04 18:53:28 -0400

    Updates db_connect.php. Adds code to reference database on public/salamanders/index.php

--- db_connect.php
+++ db_connect.php
@@ -14,10 +14,14 @@ $sql = "SELECT * FROM salamander ";
 $result = mysqli_query($db, $sql);
 
 // 3. Use returned data (if any)
+while($salamander = mysqli_fetch_assoc($result)) {
+  echo $salamander["id"] . "<br>";
+}
 
 // 4. Release returned data
 mysqli_free_result($result);
 
+// 5. Close database connection
 mysqli_close($connection);
 
 ?>
--- phase03/public/salamanders/index.php
+++ phase03/public/salamanders/index.php
@@ -2,35 +2,32 @@
 
   $result = find_all_salamanders();
 
-$salamanders = [
-  ['id' => '1',  'salamanderName' => 'Red-Legged Salamander'],
-  ['id' => '2',  'salamanderName' => 'Pigeon Mountain Salamander'],
-  ['id' => '3',  'salamanderName' => 'ZigZag Salamander'],
-  ['id' => '4',  'salamanderName' => 'Slimy Salamander'],
-];
-
 $page_title = 'Salamanders'; 
 include(SHARED_PATH . '/salamander-header.php'); 
 
 ?>
 
-<h1>Salamanders Main Page</h1>
+<h1>Salamanders</h1>
 
-  <a href="<?= url_for('/salamanders/new.php'); ?>">Create a Salamander</a>
+  <a href="<?= url_for('/salamanders/new.php'); ?>">Create Salamander</a>
 
 <table>
   <tr>
     <th>ID</th>
     <th>Name</th>
+    <th>Habitat</th>
+    <th>Desc</th>
     <th>&nbsp;</th>
     <th>&nbsp;</th>
     <th>&nbsp;</th>
   </tr>
 
-      <?php foreach($salamanders as $salamander) { ?>
+      <?php while($salamander = mysqli_fetch_assoc($result)) { ?>
         <tr>
           <td><?= h($salamander['id']); ?></td>
-    	    <td><?= h($salamander['salamanderName']); ?></td>
+    	    <td><?= h($salamander['name']); ?></td>
+          <td><?= h($salamander['habitat']); ?></td>
+          <td><?= h($salamander['description']); ?></td>
           <td><a href="<?= url_for('salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
           <td><a href="<?= url_for('salamanders/edit.php?id=' . h(u($salamander['id']))); ?>">Edit</a></td>
           <td><a href="">Delete</a></td>
