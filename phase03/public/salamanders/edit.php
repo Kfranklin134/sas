16d8eb49f7b131c71858a741d5c21aa90e481f6c
Kevin Franklin <kevinmfranklin@students.abtech.edu>
2023-11-05 11:00:38 -0500

    Modifies salamanders/edit.php to stub page

--- phase03/public/salamanders/edit.php
+++ phase03/public/salamanders/edit.php
@@ -14,19 +14,21 @@ if(is_post_request()) {
   echo "Salamander name: " . $salamanderName;
 }
 
+include(SHARED_PATH . '/salamander-header.php'); 
+
 ?>
 
 <div id = "content">
 
-  <a class="back-link" href="<?php echo url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
+  <!-- <a class="back-link" href="<?php echo url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>  -->
 
-  <h1>Edit Salamander</h1>
+  <h1>Stub for Edit Salamander</h1>
 
-  <form action="<?php echo url_for('/salamanders/edit.php?id=' . h(u($id))); ?>" method="post">
+  <!-- <form action="<?php echo url_for('/salamanders/edit.php?id=' . h(u($id))); ?>" method="post">
     <label for="salamanderName">Name</label><br>
     <input type="text" name="salamanderName" value="<?php echo $salamanderName; ?>"><br>
     <input type="submit" value="Edit Salamander">
-  </form>
+  </form> -->
 </div>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
