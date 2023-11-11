ac37018c5976b8d49da9234ed59c0ef7a7d31139
Kevin Franklin <kevinmfranklin@students.abtech.edu>
2023-11-11 17:15:02 -0500

    First commit of phase 04. Adds phase04 folders. Connects index.html to new folder and corrects paths. Hides db credentials for git.

---
+++ phase04/public/salamanders/new.php
@@ -0,0 +1,36 @@
+<?php 
+
+require_once('../../private/initialize.php');
+
+$test = $_GET['test'] ?? '';
+
+if($test == '404') {
+  error_404();
+}
+elseif($test == '500') {
+  error_500();
+}
+elseif($test == 'redirect') {
+  redirect_to(url_for('/salamanders/index.php'));
+}
+
+$page_title = 'Add New Salamander';
+include(SHARED_PATH . '/salamander-header.php');
+?>
+
+<h1>Stub for Create Salamander</h1>
+
+<!-- <div id = "content">
+
+  <a class="back-link" href="<?php echo url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
+
+  <h1>Create Salamander</h1>
+
+  <form action="<?php echo url_for('/salamanders/create.php'); ?>" method="post">
+    <label for="salamanderName">Name</label><br>
+    <input type="text" name="salamanderName"><br>
+    <input type="submit" value="Create Salamander">
+  </form>
+</div> -->
+
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
